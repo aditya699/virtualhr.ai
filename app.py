2408f571f30f2c71165e354ca2efce1d3be178d2
Aditya Bhatt <meenakshi.bhtt@gmail.com>
2024-03-26 23:32:33 +0530

    Basic V1 Web UI complete

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ Comments-
 1.Basic RAG System to communicate with your company internal Data is setup.
 (Note this is a simple QA System)
 '''
-
 import streamlit as st
 from dotenv import load_dotenv
 import os
@@ -17,6 +16,7 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain import hub
+from langchain_core.prompts import PromptTemplate
 
 # Load environment variables from .env
 load_dotenv()
@@ -25,11 +25,14 @@ load_dotenv()
 API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Define the Streamlit app
-st.title("Ask Questions about Company Policies and Procedures")
+st.title("Create Your Own Company's Question Answer System")
 
+st.image ("pic.png",width=500)
+#question_input = st.text_input("Enter your Web Path for Company Policy:", "https://docs.google.com/document/d/e/2PACX-1vS8uUx3jgheoS09Wo-JVCmuBuAbr_NeCk1TMlwHbEXbJnCoE287gdWs_Z33BIR_iSeD1aqQg4Y3BHil/pub")
+question_input ="https://docs.google.com/document/d/e/2PACX-1vS8uUx3jgheoS09Wo-JVCmuBuAbr_NeCk1TMlwHbEXbJnCoE287gdWs_Z33BIR_iSeD1aqQg4Y3BHil/pub"
 # Load, chunk and index the contents of the blog.
 loader = WebBaseLoader(
-    web_paths=("https://docs.google.com/document/d/e/2PACX-1vS8uUx3jgheoS09Wo-JVCmuBuAbr_NeCk1TMlwHbEXbJnCoE287gdWs_Z33BIR_iSeD1aqQg4Y3BHil/pub",),
+    web_paths=(question_input,),
 )
 docs = loader.load()
 
@@ -39,32 +42,33 @@ vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings
 
 # Retrieve and generate using the relevant snippets of the blog.
 retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
+template = """
+Use the following pieces of context to answer the question at the end as a Human Resource Manager.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Give all details based on context but keep the answer crisp
+
+{context}
+
+Question: {question}
+
+Helpful Answer:
+"""
+custom_rag_prompt = PromptTemplate.from_template(template)
+
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-
 def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
+    return "/n/n".join(doc.page_content for doc in docs)
 
 rag_chain = (
     {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
+    | custom_rag_prompt
     | llm
     | StrOutputParser()
 )
 
-# Example messages for prompt invocation
-example_messages = prompt.invoke(
-    {"context": "filler context", "question": "filler question"}
-).to_messages()
-
-# Get user question input
-user_question = st.text_input("Ask your question about company policies and procedures:")
-
-# Display output based on user question
-if user_question:
-    st.write("Answer:")
-    # Join the output into a single sentence
-    answer = ' '.join(chunk for chunk in rag_chain.stream(user_question))
-    st.write(answer)
+# Streamlit UI
+question_input = st.text_input("Enter your question:", "")
+if question_input:
+    answer = rag_chain.invoke(question_input)
+    st.write("Answer:", answer)
